47d2f1bd194cbd3559e4c33c705cad830c29f7b1
pohjie <0xArlet@gmail.com>
2022-10-31 19:04:54 +0800

    Implement solution for Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputStr, numRepeat) {
+  if (numRepeat < 0) {
+    return "ERROR";
+  }
 
+  repeatedStr = "";
+  for (let i=0; i<numRepeat; i++) {
+    repeatedStr += inputStr;
+  }
+
+  return repeatedStr;
 };
 
 // Do not edit below this line
