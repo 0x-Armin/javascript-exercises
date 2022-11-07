d0abc94997fcd6720bb68d23a538cece667dc096
pohjie <0xArlet@gmail.com>
2022-11-07 16:54:21 +0800

    Solve 10_Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+  idx = parseInt(idx);
 
+  if (idx < 0) return "OOPS";
+
+  let prev = 1, curr = 1;
+  let temp = 0;
+
+  for (let i=3; i <= idx; i++) {
+    temp = prev;
+    prev = curr;
+    curr = prev + temp;
+  }
+
+  return curr;
+  
 };
 
 // Do not edit below this line
