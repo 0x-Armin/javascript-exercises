91b9034dc139fbcdbcbc4c172aeeccb28d3ab38d
pohjie <0xArlet@gmail.com>
2022-11-07 17:06:00 +0800

    Solve 12_getTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+function getYearsLived(personA, personB) {
+  numALivedYears = (personA.yearOfDeath ?? new Date().getFullYear()) - personA.yearOfBirth;
+  numBLivedYears = (personB.yearOfDeath ?? new Date().getFullYear()) - personB.yearOfBirth;
 
+  if (numALivedYears > numBLivedYears) return personA;
+  else return personB;
+}
+
+const findTheOldest = function(people) {
+  return people.reduce(getYearsLived);
 };
 
 // Do not edit below this line
