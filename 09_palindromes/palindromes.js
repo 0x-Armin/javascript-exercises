cedd11f38855ab9b3535eb5313e0cd9bc2937c83
pohjie <0xArlet@gmail.com>
2022-11-07 12:47:51 +0800

    Finish palindromes problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.replace(/[^\w]|_/g, "").toLowerCase();
 
+  let left = 0, right = str.length-1;
+  while (left < right) {
+    if (str.charAt(left) != str.charAt(right)) return false;
+    left++;
+    right--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
